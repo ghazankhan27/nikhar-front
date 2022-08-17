f7a8a915dd705a16c4d28371bdaaf9eaae71d48a
ghazankhan27 <ghazankhan27>
2022-08-17 15:46:19 +0500

    made footer text appropriate

--- components/footer/subs/FooterContainer.js
+++ components/footer/subs/FooterContainer.js
@@ -1,6 +1,6 @@
 export function FooterContainer({ children }) {
   return (
-    <div className="bg-primary text-white grid grid-cols-1 md:grid-cols-3 py-10 px-20 space-y-6 md:space-y-0">
+    <div className="bg-primary text-white grid grid-cols-1 md:grid-cols-3 py-10 px-20 space-y-6 md:space-y-0 text-sm">
       {children}
     </div>
   );
--- components/navbar/subs/Logo.js
+++ components/navbar/subs/Logo.js
@@ -3,9 +3,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href={"/"}>
-      <p className="text-white text-xl md:text-2xl lg:text-3xl cursor-pointer">
-        Nikhar
-      </p>
+      <p className="text-white text-2xl lg:text-3xl cursor-pointer">Nikhar</p>
     </Link>
   );
 }
